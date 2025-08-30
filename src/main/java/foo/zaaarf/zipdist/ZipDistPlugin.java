cddad5582b9fdcdfdab96590cae64cea8ff08e85
zaaarf <me@zaaarf.foo>
2025-08-30 15:29:26 +0200

    feat: android support

--- src/main/java/foo/zaaarf/zipdist/ZipDistPlugin.java
+++ src/main/java/foo/zaaarf/zipdist/ZipDistPlugin.java
@@ -9,6 +9,10 @@ import org.gradle.api.tasks.Delete;
 import org.gradle.api.tasks.bundling.Zip;
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Objects;
+
 /**
  * The main class for the zipdist plugin.
  */
@@ -17,15 +21,45 @@ public class ZipDistPlugin implements Plugin<Project> {
 	public void apply(@NotNull Project project) {
 		project.getPluginManager().apply("maven-publish");
 
+		// setup android if needed
+		String componentName = "java";
+		if(
+			project.getPluginManager().hasPlugin("com.android.application")
+				|| project.getPluginManager().hasPlugin("com.android.library")
+		) {
+			Object androidExt = project.getExtensions().findByName("android");
+			try {
+				Method getPublishing = Objects.requireNonNull(androidExt).getClass().getMethod("getPublishing");
+				Object publishing = getPublishing.invoke(androidExt);
+
+				if(publishing != null) {
+					Method getSingleVariants = publishing.getClass().getMethod("getSingleVariants");
+					Collection<?> variants = (Collection<?>) getSingleVariants.invoke(publishing);
+
+					if(variants.isEmpty()) {
+						Method singleVariant = publishing.getClass().getMethod("singleVariant", String.class);
+						singleVariant.invoke(publishing, "release");
+						componentName = "release";
+					} else { // user wants to do things manually
+						componentName = null;
+					}
+				}
+			} catch(Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		String finalComponentName = componentName; // i hate java
 		Directory distDir = project.getLayout().getBuildDirectory().dir("dist").get();
 		Directory distRoot = distDir.dir("maven");
 		project.afterEvaluate(p -> {
 			p.getExtensions().configure(PublishingExtension.class, publishing -> {
-				if(publishing.getPublications().isEmpty()) {
+				if(publishing.getPublications().isEmpty() && finalComponentName != null) {
+					// if this was set to null then it means the user wants to do it manually
 					publishing.publications(publications -> publications.create(
 						"mavenJava",
 						MavenPublication.class,
-						publication -> publication.from(p.getComponents().findByName("java")))
+						publication -> publication.from(p.getComponents().findByName(finalComponentName)))
 					);
 				}
 
